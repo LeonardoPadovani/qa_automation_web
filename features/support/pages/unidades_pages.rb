63f0eddfc2c09890004d34a93cdcba007a2acf40
leonardo.padovani <leonardo.padovani@engegraph.com.br>
2020-08-19 22:03:00 -0300

    comentarios

--- features/support/pages/unidades_pages.rb
+++ features/support/pages/unidades_pages.rb
@@ -1,3 +1,5 @@
+#page object Unidade criada para reuso de codigo seguindo os padroes
+
 class UnidadePage
     include Capybara::DSL
 
@@ -11,11 +13,13 @@ class UnidadePage
 
     def selecionarFacilidade(filtro)
         abrirFiltroFacilidade
+        #aqui tentei pelo value e pelo name mas nao consegui clicar. Tentei pelo label, mas os checks com espaço não foram possiveis clicar 
         #find("label[for=_#{filtro}]").click
         find(".checkbox-selectcomponentstyle__CheckboxSelectScrollStyled-sc-7ktrvg-4 > div:nth-child(#{filtro})")
     end
 
     def visualizarDetalhes
+        # criei dessa forma para que sempre seja clicado no primeiro Ver Detalhes que aparecer. Ficou dinamico independente do check marcado
         find(".sc-bwzfXH.gridcomponent__RowStyled-sc-8zg10d-1.eYPaKj  > div:nth-child(1)> div:nth-child(1) > div:nth-child(5) > div:nth-child(2)").click
     end
 
